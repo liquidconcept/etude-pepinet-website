dc7b195df99df945f28f8d419f44886117bc0d6b
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2011-12-01 16:34:05 +0100

    add fixed background

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,24 @@
 //= require jquery
 //= require jquery_ujs
 //= require underscore
+
+$(function() {
+
+  var backgroundResize = function() {
+    var imageWidth   = $('.background').width(),
+        imageHeight  = $('.background').height(),
+        windowWidth  = $(window).width(),
+        windowHeight = $(window).height(),
+        widthRatio   = windowWidth  / imageWidth,
+        heightRatio  = windowHeight / imageHeight;
+
+    if (widthRatio < heightRatio) {
+      $('.background').css({ width: 'auto', height: '100%', left: (windowWidth - imageWidth) / 2 + 'px' });
+    } else {
+      $('.background').css({ width: '100%', height: 'auto', left: '0px' });
+    }
+  }
+
+  backgroundResize();
+  $(window).on('resize', backgroundResize);
+});

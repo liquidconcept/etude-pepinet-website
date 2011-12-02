d6543951fd492f212b33daefa0224f6491eac0dc
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2011-12-02 09:58:43 +0100

    link to page with current status

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,12 +1,17 @@
 class PageController < ApplicationController
   def show
-    @page = params[:page] || 'home'
-
-    if @page
-      render :template => "page/#{@page}"
+    # TODO: check if template exist
+    if current_static_page
+      render :template => "page/#{current_static_page}"
     else
       # TODO: render not found page
       render text: '404', status: 404
     end
+  end
+
+  private
+
+  def current_static_page
+    @page = (controller_name == 'page' ? (params[:page] || 'home') : nil)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def link_to_with_active(name, options = {}, html_options = {}, &block)
+    html_options[:class] = [html_options[:class], 'active'].join(' ') if current_page?(options)
+
+    link_to(name, options, html_options, &block)
+  end
 end

667cf37c6367361674bfb7bd8a2a7ea2da28eeb5
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2014-05-28 17:16:41 +0200

    Integarte lib LatinToInteger

--- app/controllers/book_updates_controller.rb
+++ app/controllers/book_updates_controller.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 
 class BookUpdatesController < ApplicationController
+  include LatinToInteger
   before_filter :protected_page
 
   def index
-    @book_updates = BookUpdates.all
+    @book_updates = BookUpdates.all.sort {|a, b| latin_to_integer(a.chapter) <=> latin_to_integer(b.chapter)}
   end
 end
